575dd89591071a5be751c3c48000a363db7aa9ef
jacobduron90 <jacobduron@gmail.com>
2016-10-16 16:44:05 -0500

    Moving DashboardModule logic into app module.

--- app/src/main/java/com/android/brewnotes/Modules.java
+++ app/src/main/java/com/android/brewnotes/Modules.java
@@ -4,7 +4,6 @@ package com.android.brewnotes;
  * Created by jacobduron on 10/12/16.
  */
 
-import com.android.brewnotes.dashboard.DashboardModule;
 import com.android.brewnotes.framework.ApplicationModule;
 
 
@@ -13,7 +12,6 @@ public class Modules {
     public static Object[] list(){
         Object[] moduleList =
                 new Object[]{
-                        new DashboardModule(),
                         new ApplicationModule()
                 };
         return moduleList;
--- app/src/main/java/com/android/brewnotes/framework/ApplicationModule.java
+++ app/src/main/java/com/android/brewnotes/framework/ApplicationModule.java
@@ -2,6 +2,8 @@ package com.android.brewnotes.framework;
 
 import com.android.brewnotes.coffeebag.CoffeeBagActivity;
 import com.android.brewnotes.coffeebag.CoffeeBagDetailActivity;
+import com.android.brewnotes.dashboard.CompanyAdapter;
+import com.android.brewnotes.dashboard.DashboardActivity;
 import com.android.brewnotes.login.LoginPresenter;
 import com.android.brewnotes.service.CoffeeBagManager;
 import com.android.brewnotes.ErrorHandler;
@@ -9,10 +11,16 @@ import com.android.brewnotes.login.MainActivity;
 import com.android.brewnotes.service.BrewNotesContract;
 import com.android.brewnotes.service.UserManager;
 
+import javax.inject.Named;
 import javax.inject.Singleton;
 
 import dagger.Module;
 import dagger.Provides;
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 /**
  * Created by jacobduron on 10/12/16.
@@ -24,7 +32,8 @@ import dagger.Provides;
                 MainActivity.class,
                 CoffeeBagActivity.class,
                 CoffeeBagDetailActivity.class,
-                ErrorHandler.class
+                ErrorHandler.class,
+                DashboardActivity.class,
 
         },
         staticInjections = {
@@ -44,6 +53,35 @@ public class ApplicationModule {
     @Singleton
     CoffeeBagManager providesCoffeeBagManager(UserManager userManager, BrewNotesContract contract){
         return new CoffeeBagManager(userManager, contract);
+    }
+
+    @Provides
+    CompanyAdapter provideCompanyAdapter(){
+        return new CompanyAdapter();
+    }
+
+    @Provides
+    BrewNotesContract providesBrewContract(Retrofit restAdapter){
+        return restAdapter.create(BrewNotesContract.class);
+    }
+
+    @Provides @Named("authToken") String providesAuthToken(UserManager um){
+        return um.getAuthToken();
+    }
+
+    @Provides
+    @Singleton
+    Retrofit providesRetrofit(){
+        HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
+        interceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+        OkHttpClient client = new OkHttpClient.Builder().addInterceptor(interceptor).build();
+
+        return new Retrofit.Builder()
+                .baseUrl("http://104.131.83.239/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
+                .client(client)
+                .build();
     }
 
 }
